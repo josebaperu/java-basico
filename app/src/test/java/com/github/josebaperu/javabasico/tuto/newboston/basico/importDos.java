5e300d76ff3ade5a95647658ca2054f19298747a
carlos <carlosaugusto200004@gmail.com>
2022-12-31 11:09:51 -0500

    newboston-basico: primer commit

--- app/src/test/java/com/github/josebaperu/javabasico/tuto/newboston/basico/importDos.java
+++ app/src/test/java/com/github/josebaperu/javabasico/tuto/newboston/basico/importDos.java
@@ -1,4 +1,19 @@
 package com.github.josebaperu.javabasico.tuto.newboston.basico;
+import com.github.josebaperu.javabasico.model.Human;
+import com.github.josebaperu.javabasico.test.strings.StringTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@DisplayName("importaciones")
 public class importDos {
+    private static final Logger LOG = Logger.getLogger(StringTest.class.getName());
+
+    @Test
+    @DisplayName("Ejercicio : creacion objeto")
+    public void Alain() {
+
+    }
 }
