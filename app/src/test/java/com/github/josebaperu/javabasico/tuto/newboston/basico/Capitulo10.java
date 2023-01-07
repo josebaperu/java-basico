91c54919e23a4765d98fabd347ef1275ec4a0c25
carlos <carlosaugusto200004@gmail.com>
2023-01-06 20:41:41 -0500

    newboston-basico: primer commit

--- app/src/test/java/com/github/josebaperu/javabasico/tuto/newboston/basico/Capitulo10.java
+++ app/src/test/java/com/github/josebaperu/javabasico/tuto/newboston/basico/Capitulo10.java
@@ -1,6 +1,7 @@
 package com.github.josebaperu.javabasico.tuto.newboston.basico;
 
 import com.github.josebaperu.javabasico.test.strings.StringTest;
+import com.github.josebaperu.javabasico.tuto.newboston.intermedio.cositas;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,7 @@ import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.random.RandomGenerator;
+
 @DisplayName("Ejercicios : String")
 public class Capitulo10 {
     private static final Logger LOG = Logger.getLogger(Capitulo10.class.getName());
@@ -130,123 +132,162 @@ public class Capitulo10 {
 
 
     }
+
     @Test
     @DisplayName("Ejercicio : Ninth")
     public void stringTestNinth() {
         int counter = 0;
-        while (counter < 100){
+        while (counter < 100) {
             System.out.println(counter);
             counter++;
         }
     }
+
     @Test
     @DisplayName("Ejercicio : Tenth")
     public void stringTestTenth() {
         System.out.println("esto es una simple clase");
 
     }
+
     @Test
     @DisplayName("Ejercicio : Eleventh")
     public void stringTestEleventh(String name) {
-            System.out.println("Hola " + name);
+        System.out.println("Hola " + name);
 
     }
+
     @Test
     @DisplayName("Ejercicio : Twelfth")
     public void stringTestTwelfth() {
         Random dice = new Random();
         int number;
 
-        for (int counter=1; counter <=10; counter++){
-            number = 1+dice.nextInt(6);
+        for (int counter = 1; counter <= 10; counter++) {
+            number = 1 + dice.nextInt(6);
             System.out.println(number + " ");
         }
 
 
     }
+
     @Test
     @DisplayName("Ejercicio : Thirteenth")
     public void stringTestThirteenth() {
-        int bucky [] = new int[10];
-        bucky[0]=87;
-        bucky[1]=543;
-        bucky[9]=65;
+        int bucky[] = new int[10];
+        bucky[0] = 87;
+        bucky[1] = 543;
+        bucky[9] = 65;
         System.out.println(bucky[9]);
     }
+
     //esto es lo más básico
     @Test
     @DisplayName("Ejercicio : FourTeenth")
     public void stringTestFourteenth() {
-        int bucky [] = {2,4,5,7,9};
+        int bucky[] = {2, 4, 5, 7, 9};
 
         System.out.println(bucky[2]);
     }
+
     @Test
     @DisplayName("Ejercicio : Fifteenth")
     public void stringTestFifteenth() {
         System.out.println("Index\tValue");
-        int bucky [] = {32,12,18,54,2};
-        for (int counter =0;counter< bucky.length;counter++){
-            System.out.println(1 + counter + "\t" + bucky [counter]);
+        int bucky[] = {32, 12, 18, 54, 2};
+        for (int counter = 0; counter < bucky.length; counter++) {
+            System.out.println(1 + counter + "\t" + bucky[counter]);
         }
 
     }
+
     @Test
     @DisplayName("Ejercicio : Sixteenth")
     public void stringTestSixteenth() {
-        int bucky[] = {21,16,81,21,3};
+        int bucky[] = {21, 16, 81, 21, 3};
         int sum = 0;
 
-        for (int counter=0;counter<bucky.length;counter++){
-            sum+=bucky[counter];
+        for (int counter = 0; counter < bucky.length; counter++) {
+            sum += bucky[counter];
         }
         System.out.println("La suma de esos numeros son " + sum);
     }
+
     @Test
     @DisplayName("Ejercicio : Eightteenth")
     public void stringTestEightteenth() {
         Random rand = new Random();
         int freq[] = new int[7];
-        for (int roll=1;roll<1000;roll++){
-            ++freq[1+rand.nextInt(6)];
+        for (int roll = 1; roll < 1000; roll++) {
+            ++freq[1 + rand.nextInt(6)];
         }
         System.out.println("Face\tFrecuency");
-        for (int face = 1;face<freq.length;face++){
-            System.out.println(face+"\t"+freq[face]);
+        for (int face = 1; face < freq.length; face++) {
+            System.out.println(face + "\t" + freq[face]);
         }
     }
+
     @Test
     @DisplayName("Ejercicio : Nineteenth")
     public void stringTestNineteenth() {
-        int bucky[] = {3,4,5,6,7};
-        int total=1;
+        int bucky[] = {3, 4, 5, 6, 7};
+        int total = 1;
 
-        for (int x: bucky){
+        for (int x : bucky) {
             total += x;
         }
-            System.out.println(total);
+        System.out.println(total);
     }
+
     @Test
     @DisplayName("Ejercicio : Twentieth")
     public void stringTestTwentieth() {
-        int bucky[] = {3,4,5,6,7};
+        int bucky[] = {3, 4, 5, 6, 7};
         change(bucky);
 
-        for (int y:bucky)
+        for (int y : bucky)
             System.out.println(y);
     }
-    public  void change (int x[]){
-        for(int counter =0;counter<x.length;counter++)
-            x[counter]+=5;
+
+    public void change(int x[]) {
+        for (int counter = 0; counter < x.length; counter++)
+            x[counter] += 5;
 
     }
+
     @Test
     @DisplayName("Ejercicio : Twentyoneth")
     public void stringTestTwentyfirstth() {
-        int firstarray[][]={{8,9,10,11},{12,13,14,15}};
-        int secondarray[][]={{30,31,32,33},{43},{4,5,6}};
+        int firstarray[][] = {{8, 9, 10, 11}, {12, 13, 14, 15}};
+        int secondarray[][] = {{30, 31, 32, 33}, {43}, {4, 5, 6}};
 
 
     }
+
+    @Test
+    @DisplayName("Ejercicio : TwentySecondth")
+    public void stringTestTwentySecondth() {
+        System.out.println(avarage(43,56,76,8,65,76,2,31));
     }
+
+    public  int avarage(int...numbers){
+        int total=0;
+        for (int x:numbers);
+
+        return  total/numbers.length;
+
+    }
+    //me sale incorreccto "total+=x:" en la linea274
+
+    @Test
+    @DisplayName("Ejercicio : TwentyThird")
+    public void stringTestTwentyThird() {
+       cositas cositasObject= new cositas();
+        System.out.println(cositasObject.toMilitary());
+        cositasObject.setTime(13,27,6);
+        System.out.println(cositasObject.toMilitary());
+
+    }
+}
+
 
